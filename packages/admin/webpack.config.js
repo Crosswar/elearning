1285001051506827a00ed7da4414c72a6d03cfad
Felippe Rodrigo Puhle <lippezzin@gmail.com>
2019-05-29 20:13:25 -0300

    add dotenv

--- packages/admin/webpack.config.js
+++ packages/admin/webpack.config.js
@@ -5,6 +5,9 @@ const config = require('@ibsel/webpack')
 module.exports = config({
   entry: './src/index.tsx',
   output: resolve(__dirname, 'dist'),
+  dotenv: {
+    path: resolve(__dirname, '.env'),
+  },
   html: {
     template: resolve(__dirname, 'public', 'index.html'),
     favicon: resolve(__dirname, 'public', 'favicon.ico'),
--- packages/webpack/index.js
+++ packages/webpack/index.js
@@ -1,6 +1,7 @@
 const webpack = require('webpack')
 const { WebpackPluginServe } = require('webpack-plugin-serve')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const Dotenv = require('dotenv-webpack')
 
 const isDev = process.env.NODE_ENV === 'development'
 
@@ -8,7 +9,7 @@ const getEntry = ({ entry }) => {
   return isDev ? [entry, 'webpack-plugin-serve/client'] : entry
 }
 
-const getPlugins = ({ output, html }) => {
+const getPlugins = ({ output, dotenv, html }) => {
   const plugins = [
     new HtmlWebpackPlugin(html),
     new webpack.DefinePlugin({
@@ -17,6 +18,15 @@ const getPlugins = ({ output, html }) => {
       },
     }),
   ]
+
+  if (dotenv) {
+    plugins.push(
+      new Dotenv({
+        ...dotenv,
+        safe: true,
+      })
+    )
+  }
 
   if (isDev) {
     plugins.push(
@@ -32,7 +42,7 @@ const getPlugins = ({ output, html }) => {
   return plugins
 }
 
-module.exports = ({ entry, output, html }) => ({
+module.exports = ({ entry, output, dotenv, html }) => ({
   entry: getEntry({ entry }),
   mode: process.env.NODE_ENV,
   devtool: isDev ? 'cheap-eval-source-map' : false,
@@ -61,6 +71,6 @@ module.exports = ({ entry, output, html }) => ({
     publicPath: '/',
     filename: !isDev ? 'bundle.[contenthash].js' : 'bundle.js',
   },
-  plugins: getPlugins({ output, html }),
+  plugins: getPlugins({ output, dotenv, html }),
   watch: isDev,
 })
