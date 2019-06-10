a867373a7c7c4a5d1756580790d82dd99d7febd3
Felippe Rodrigo Puhle <lippezzin@gmail.com>
2019-06-10 08:48:23 -0300

    add deleteUser mutation

--- packages/admin/src/routes/Users/List/UsersList.tsx
+++ packages/admin/src/routes/Users/List/UsersList.tsx
@@ -1,7 +1,8 @@
 import * as React from 'react'
 import { Helmet } from 'react-helmet'
-import { Query } from 'react-apollo'
+import { Mutation, Query } from 'react-apollo'
 
+import { extractApolloError } from '@ibsel/core/helpers/apollo'
 import {
   Button,
   Card,
@@ -11,11 +12,16 @@ import {
 import { Dialog, Notification } from '@ibsel/admin/src/contexts'
 
 import {
+  DeleteUserMutation,
+  DeleteUserMutationVariables,
+} from './__generated__/DeleteUserMutation'
+import {
   UsersListQuery_usersList,
   UsersListQuery,
   UsersListQueryVariables,
 } from './__generated__/UsersListQuery'
-import SITE_TEMPLATE_QUERY from './UsersListQuery.graphql'
+import DELETE_USER_MUTATION from './DeleteUserMutation.graphql'
+import USERS_LIST_QUERY from './UsersListQuery.graphql'
 
 const UsersList = () => {
   const dialogs = React.useContext(Dialog.Context)
@@ -30,76 +36,83 @@ const UsersList = () => {
       <Helmet title='IBSEL Admin | Users list' />
 
       <Query<UsersListQuery, UsersListQueryVariables>
-        query={SITE_TEMPLATE_QUERY}
+        query={USERS_LIST_QUERY}
         variables={{ page, size, search }}
       >
-        {({ data, loading }) => (
-          <Card>
-            <Card.Header.Icon
-              icon={<MaterialIcon>people</MaterialIcon>}
-              title='Users list'
-            />
-            <Card.Body>
-              <DataTable<UsersListQuery_usersList>
-                loading={loading}
-                data={data ? data.usersList : []}
-                total={data ? data.usersCount : 0}
-                size={size}
-                onSearch={setSearch}
-                onPaginate={setPage}
-                columns={[
-                  {
-                    key: 'name',
-                    header: 'Name',
-                  },
-                  {
-                    key: 'email',
-                    header: 'Email',
-                  },
-                ]}
-                renderActions={({ name }) => (
-                  <>
-                    <Button
-                      mode={Button.Mode.TRANSPARENT}
-                      color={Button.Color.SUCCESS}
-                      size={Button.Size.SMALL}
-                      fab
-                    >
-                      <Button.Icon>edit</Button.Icon>
-                    </Button>
+        {({ data, loading, refetch }) => (
+          <Mutation<DeleteUserMutation, DeleteUserMutationVariables>
+            mutation={DELETE_USER_MUTATION}
+            onCompleted={result => {
+              refetch()
+            }}
+            onError={error => {
+              notifications.error(extractApolloError(error))
+            }}
+          >
+            {deleteUser => (
+              <Card>
+                <Card.Header.Icon
+                  icon={<MaterialIcon>people</MaterialIcon>}
+                  title='Users list'
+                />
+                <Card.Body>
+                  <DataTable<UsersListQuery_usersList>
+                    loading={loading}
+                    data={data ? data.usersList : []}
+                    total={data ? data.usersCount : 0}
+                    size={size}
+                    onSearch={setSearch}
+                    onPaginate={setPage}
+                    columns={[
+                      {
+                        key: 'name',
+                        header: 'Name',
+                      },
+                      {
+                        key: 'email',
+                        header: 'Email',
+                      },
+                    ]}
+                    renderActions={user => (
+                      <>
+                        <Button
+                          mode={Button.Mode.TRANSPARENT}
+                          color={Button.Color.SUCCESS}
+                          size={Button.Size.SMALL}
+                          fab
+                        >
+                          <Button.Icon>edit</Button.Icon>
+                        </Button>
 
-                    <Button
-                      mode={Button.Mode.TRANSPARENT}
-                      color={Button.Color.DANGER}
-                      size={Button.Size.SMALL}
-                      fab
-                      onClick={() =>
-                        dialogs.confirm({
-                          title: 'Are you sure?',
-                          body: (
-                            <>
-                              <u>{name}</u> will be removed from the database
-                              and you won't be able to revert this!
-                            </>
-                          ),
-                          okLabel: 'Yes, delete it',
-                          onOk: () => {
-                            notifications.success(
-                              <>
-                                <b>{name}</b> was successfully removed
-                              </>
-                            )
-                          },
-                        })
-                      }
-                    >
-                      <Button.Icon>close</Button.Icon>
-                    </Button>
-                  </>
-                )}
-              />
-            </Card.Body>
-          </Card>
+                        <Button
+                          mode={Button.Mode.TRANSPARENT}
+                          color={Button.Color.DANGER}
+                          size={Button.Size.SMALL}
+                          fab
+                          onClick={() =>
+                            dialogs.confirm({
+                              title: 'Are you sure?',
+                              body: (
+                                <>
+                                  <u>{name}</u> will be removed from the
+                                  database and you won't be able to revert this!
+                                </>
+                              ),
+                              okLabel: 'Yes, delete it',
+                              onOk: () =>
+                                deleteUser({ variables: { id: user._id } }),
+                            })
+                          }
+                        >
+                          <Button.Icon>close</Button.Icon>
+                        </Button>
+                      </>
+                    )}
+                  />
+                </Card.Body>
+              </Card>
+            )}
+          </Mutation>
         )}
       </Query>
     </>
